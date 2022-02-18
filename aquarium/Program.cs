6ce2d6d1a70736a99f263f791baefb66e3b51905
avorobeov <96600048+avorobeov@users.noreply.github.com>
2022-02-18 20:45:32 +0300

    Добавьте файлы проекта.

---
+++ aquarium/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aquarium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Fish
+    {
+        public int Age { get; private set; }
+        public int MaxAge { get; private set; }
+
+        public Fish(int age,int maxAge)
+        {
+            Age = age;
+            MaxAge = maxAge;
+        }
+    }
+}
